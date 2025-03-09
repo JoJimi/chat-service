0c3a5c1ab709c970e97b7b4af5aa8a7b1ef00ea4
duddnddnfl <duddnddnfl@naver.com>
2025-03-10 01:47:19 +0900

    [feat/#9] Dto 추가

---
+++ src/main/java/org/example/chatservice/domain/chatroom/dto/ChatroomDto.java
@@ -0,0 +1,20 @@
+package org.example.chatservice.domain.chatroom.dto;
+
+import org.example.chatservice.domain.chatroom.domain.Chatroom;
+
+import java.time.LocalDateTime;
+
+public record ChatroomDto(
+        Long id,
+        String title,
+        Integer memberCount,
+        LocalDateTime createdAt) {
+
+    public static ChatroomDto from(Chatroom chatroom){
+        return new ChatroomDto(
+                chatroom.getId(),
+                chatroom.getTitle(),
+                chatroom.getMemberChatroomMappingSet().size(),
+                chatroom.getCreatedAt());
+    }
+}
