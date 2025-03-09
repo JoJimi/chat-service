d9b56f41f70d8269e47638e96118f7274b81dda9
duddnddnfl <duddnddnfl@naver.com>
2025-03-09 15:42:32 +0900

    [feat/#9] BaseEntity 추가

--- src/main/java/org/example/chatservice/domain/member/entity/Member.java
+++ src/main/java/org/example/chatservice/domain/member/entity/Member.java
@@ -2,16 +2,19 @@ package org.example.chatservice.domain.member.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.example.chatservice.domain.chatroom.domain.MemberChatroomMapping;
+import org.example.chatservice.global.entity.BaseEntity;
 import org.example.chatservice.type.GenderType;
 
 import java.time.LocalDate;
+import java.util.Set;
 
 @Getter
 @Entity
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Member {
+public class Member extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,5 +28,6 @@ public class Member {
     private LocalDate birthDay;
     private String role;
 
-
+    @OneToMany(mappedBy = "member")
+    Set<MemberChatroomMapping> memberChatroomMappingSet;
 }
