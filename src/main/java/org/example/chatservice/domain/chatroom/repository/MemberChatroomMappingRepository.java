d82de3175a862e11eb67328076d130a620d7cdeb
duddnddnfl <duddnddnfl@naver.com>
2025-03-09 17:04:13 +0900

    [feat/#9] 채팅방 기능 로직 구현

--- src/main/java/org/example/chatservice/domain/chatroom/repository/MemberChatroomMappingRepository.java
+++ src/main/java/org/example/chatservice/domain/chatroom/repository/MemberChatroomMappingRepository.java
@@ -3,5 +3,12 @@ package org.example.chatservice.domain.chatroom.repository;
 import org.example.chatservice.domain.chatroom.domain.MemberChatroomMapping;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MemberChatroomMappingRepository extends JpaRepository<MemberChatroomMapping, Long> {
+    Boolean existByMemberIdAndChatroomId(Long memberId, Long chatroomId);
+
+    void deleteByMemberIdAndChatroomId(Long memberId, Long chatroomId);
+
+    List<MemberChatroomMapping> findAllByMemberId(Long memberId);
 }
