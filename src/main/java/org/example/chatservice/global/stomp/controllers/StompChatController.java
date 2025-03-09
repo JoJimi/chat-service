b6ffea7112fe854a7211c0e25a786bb90d14ef5f
duddnddnfl <duddnddnfl@naver.com>
2025-03-10 02:15:54 +0900

    [feat/#11] 메시지 저장 로직 구현

--- src/main/java/org/example/chatservice/global/stomp/controllers/StompChatController.java
+++ src/main/java/org/example/chatservice/global/stomp/controllers/StompChatController.java
@@ -1,20 +1,28 @@
 package org.example.chatservice.global.stomp.controllers;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.example.chatservice.domain.chatroom.service.ChatService;
+import org.example.chatservice.domain.message.entity.Message;
+import org.example.chatservice.global.oauth2.custom.CustomOAuth2User;
 import org.example.chatservice.global.stomp.dto.ChatMessage;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
 
 import java.security.Principal;
 import java.util.Map;
 
 @Slf4j
+@RequiredArgsConstructor
 @Controller
 public class StompChatController {
+
+    private final ChatService chatService;
 
     @MessageMapping("/chats/{chatroomId}")
     @SendTo("/sub/chats/{chatroomId}")
@@ -22,7 +30,10 @@ public class StompChatController {
                                      @DestinationVariable Long chatroomId,
                                      @Payload Map<String, String> payload) {
         log.info("{} sent {} in {}", principal.getName(), payload, chatroomId);
+        CustomOAuth2User user = (CustomOAuth2User) ((OAuth2AuthenticationToken) principal).getPrincipal();
 
+        Message message = chatService.saveMessage(user.getMember(), chatroomId, payload.get("message"));
         return new ChatMessage(principal.getName(), payload.get("message"));
+
     }
 }
